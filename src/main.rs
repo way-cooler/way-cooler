e8725f11e643ddda0c43f3540eb2feb3e6eeea60
Snirk Immington <snirk.immington@gmail.com>
2016-01-21 15:52:56 -0500

    Added example translation from wlc example, does not compile

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 extern crate rustwlc;
 
+struct Data {
+    handle: rustwlc::WLCHandle,
+    grab: rustwlc::Point,
+    drag_edge: rustwlc::ResizeEdge
+}
+
+pub data: mut 'static Data;
+
+fn start_interactive_action(view: rustwlc::WLCHandle, origin: rustwlc::Point) -> bool
+{
+    data.handle = view;
+    data.grab = origin;
+    view.bring_to_front();
+    return true;
+}
+
 fn main() {
     println!("Hello, world!");
-    //rustwlc::init();
+    
 }
