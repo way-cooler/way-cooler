8997ee79dbf0b6bb0f641417f3b9a24cd7c47c90
snirk <snirk.immington@gmail.com>
2016-04-02 21:01:26 -0400

    Add types to registry

--- src/registry.rs
+++ src/registry.rs
@@ -1,27 +1,61 @@
 //! way-cooler registry.
 
 use std::collections::HashMap;
-use std::sync::RwLock;
+use std::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
+
+pub type RegKey = String;
+pub type RegVal = RegistryValue;
+pub type RegMap = HashMap<RegKey, RegVal>;
 
 lazy_static! {
     /// Registry variable for the registry
-    static ref REGISTRY: RwLock<HashMap<String, RegistryValue>> =
+    static ref REGISTRY: RwLock<RegMap> =
         RwLock::new(HashMap::new());
 }
 
+/// A value in the registry.
 #[derive(Debug, Clone)]
 pub enum RegistryValue {
+    /// An integer value
     Integer(i32),
+    /// A floating point value
     Float(f32),
+    /// A boolean value
     Boolean(bool),
-    String(String)
+    /// A string value
+    String(String),
+    /// A list of values.
+    List(Vec<RegistryValue>)
 }
 
-pub fn get<'a>(name: &String) -> Option<RegistryValue> {
-    let mut val: Option<RegistryValue> = None;
+/// Acquires a read lock on the registry.
+pub fn read_lock<'a>()
+                     -> RwLockReadGuard<'a, RegMap> {
+    REGISTRY.read().unwrap()
+}
+
+/// Acquires a write lock on the registry.
+pub fn write_lock<'a>()
+                      -> RwLockWriteGuard<'a, RegMap> {
+    REGISTRY.write().unwrap()
+}
+
+/// Gets a value from the regsitry.
+pub fn get(name: &RegKey) -> Option<RegVal> {
+    let mut val: Option<RegistryValue>;
     {
         let reg = REGISTRY.read().unwrap();
         val = reg.get(name).cloned();
     }
     val
 }
+
+/// Gets a value from the registry, or a default
+/// if the value is not found.
+pub fn get_or_default(name: &RegKey, value: RegVal) -> RegVal {
+    match get(name) {
+        Some(value) => value,
+        None => value
+    }
+}
+
