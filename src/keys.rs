4e20e56c5e285ffd356ed8212d33201816b12359
snirk <snirk.immington@gmail.com>
2016-04-02 23:35:30 -0400

    Add KeyPress::from_key_names

--- src/keys.rs
+++ src/keys.rs
@@ -2,7 +2,7 @@
 
 use std::collections::{HashMap, HashSet};
 use std::sync::{RwLock};
-use rustwlc::xkb::Keysym;
+use rustwlc::xkb::{Keysym, NameFlags};
 use rustwlc::types::*; // Need * for bitflags...
 use std::hash::{Hash, Hasher};
 
@@ -22,21 +22,44 @@ pub fn keymod_from_names(keys: Vec<&str>) -> Result<KeyMod, String> {
     let mut result = KeyMod::empty();
     for key in keys {
         match key.to_lowercase().as_str() {
-            "shift" => result = result | MOD_SHIFT,
+            "shift"            => result = result | MOD_SHIFT,
             "control" | "ctrl" => result = result | MOD_CTRL,
-            err => return Err(format!("Invalid character: {}", err))
+            "alt"              => result = result | MOD_ALT,
+            "mod2"             => result = result | MOD_MOD2,
+            "mod3"             => result = result | MOD_MOD3,
+            "mod4" | "super" | "logo" => result = result | MOD_MOD4,
+            "mod5" | "5mod5me" => result = result | MOD_MOD5,
+            err => return Err(format!("Invalid modifier: {}", err))
         }
     }
     return Ok(result);
 }
 
 impl KeyPress {
-    pub fn from_key_state() {
-        
-    }
-
-    pub fn from_key_names(mods: Vec<String>, keys: Vec<String>) -> KeyPress {
-        
+    /// Creates a new KeyPress struct from a list of modifier and key names
+    pub fn from_key_names(mods: Vec<&str>, keys: Vec<&str>) -> Result<KeyPress, String> {
+        match keymod_from_names(mods) {
+            Err(message) => Err(message),
+            Ok(mods) => {
+                let mut syms: Vec<Keysym> = Vec::with_capacity(keys.len());
+                for name in keys {
+                    if let Some(sym) = Keysym::from_name(name.to_string(),
+                                                         NameFlags::None) {
+                            syms.push(sym);
+                    }
+                    // Else if could not parse
+                    else if let Some(sym) = Keysym::from_name(name.to_string(),
+                                                        NameFlags::CaseInsensitive) {
+                        syms.push(sym);
+                    }
+                    else {
+                        return Err(format!("Invalid key: {}", name));
+                    }
+                }
+                syms.sort_by_key(|s| s.get_code());
+                return Ok(KeyPress { modifiers: mods, keys: syms });
+            }
+        }
     }
 }
 
