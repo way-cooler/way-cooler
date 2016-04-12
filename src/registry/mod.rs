9da1dd07483cb5593ab6ac2724dba2f3ea39ae7d
snirk <snirk.immington@gmail.com>
2016-04-12 19:31:07 -0400

    Changed registry types to hold JSON data

--- src/registry/mod.rs
+++ src/registry/mod.rs
@@ -4,10 +4,10 @@ use std::collections::HashMap;
 use std::sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 use rustc_serialize::{Encodable, Decodable};
-use rustc_serialize::json::{Json, ToJson};
+use rustc_serialize::json;
+use rustc_serialize::json::{Json, ToJson, EncoderError};
 
 mod types;
-//use self::types::*;
 
 pub use self::types::{RegistryAccess, RegistryValue};
 
@@ -17,6 +17,13 @@ lazy_static! {
     /// Registry variable for the registry
     static ref REGISTRY: RwLock<RegMap> =
         RwLock::new(HashMap::new());
+}
+
+/// Error types that can happen
+#[derive(Debug, PartialEq, Eq)]
+pub enum RegistryError {
+    InvalidJson,
+    KeyNotFound
 }
 
 /// Acquires a read lock on the registry.
@@ -30,13 +37,36 @@ fn write_lock<'a>() -> RwLockWriteGuard<'a, RegMap> {
 }
 
 /// Gets a Json object from a registry key
-pub fn get_json(name: &String) -> Option<Json> {
+pub fn get_json(name: &String) -> Option<Arc<Json>> {
     trace!("get_json: {}", name);
     let ref reg = *read_lock();
-    if let Some(ref val) = reg.get(name) {
+    if let Some(val) = reg.get(name) {
         Some(val.get_json())
     }
     else {
         None
     }
+}
+
+use std::ops::Deref;
+
+pub fn get<T: Decodable>(name: &String) -> Result<T, RegistryError> {
+    let maybe_json = get_json(name);
+    if let Some(json_arc) = maybe_json {
+        let mut decoder = json::Decoder::new(json_arc.deref().to_json());
+        match T::decode(&mut decoder) {
+            Ok(val) => Ok(val),
+            Err(e) => Err(RegistryError::InvalidJson)
+        }
+    }
+    else {
+        Err(RegistryError::KeyNotFound)
+    }
+}
+
+pub fn set<T: ToJson>(key: String, val: T) {
+    trace!("set: {}", key);
+    let ref mut write_reg = *write_lock();
+    let regvalue = RegistryValue::new(RegistryAccess::Public, val);
+    write_reg.insert(key, regvalue);
 }
--- src/registry/types.rs
+++ src/registry/types.rs
@@ -2,9 +2,11 @@
 
 use std::fmt::{Debug, Display, Formatter};
 
+use std::sync::Arc;
 use rustc_serialize::{Encodable, Decodable};
 use rustc_serialize::json::{Json, ToJson};
 
+/// How much access things have
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum RegistryAccess {
     Public,
@@ -15,124 +17,32 @@ pub enum RegistryAccess {
 /// Values stored in the registry
 pub struct RegistryValue {
     access: RegistryAccess,
-    object: Arc<ToJson + Sync + Send>
+    object: Arc<Json>
+}
+
+//#[derive(RustcSeralizeable)]
+struct Point {
+    x: u32,
+    y: u32
 }
 
 impl RegistryValue {
+
+    pub fn new<T>(access: RegistryAccess, data: T) -> RegistryValue
+        where T: ToJson  {
+        RegistryValue {
+            access: access,
+            object: Arc::new(data.to_json())
+        }
+    }
+
     /// What access the module has to it
     pub fn access(&self) -> RegistryAccess {
         self.access
     }
 
     /// Gets the json of a registry value
-    pub fn get_json(&self) -> Json {
-        self.object.to_json()
-    }
-}
-
-pub struct JsonWrap(Json);
-
-use hlua::{Push, PushGuard, AsMutLua, LuaContext};
-use hlua::LuaTable;
-use hlua_ffi;
-
-fn push_iter<L, V, I>(mut lua: L, iterator: I) -> PushGuard<L>
-    where L: AsMutLua, V: for<'b> Push<&'b mut L>, I: Iterator<Item=V>
-{
-    // creating empty table
-    unsafe { hlua_ffi::lua_newtable(lua.as_mut_lua().0) };
-
-    for (elem, index) in iterator.zip((1 ..)) {
-        let size = elem.push_to_lua(&mut lua).forget();
-
-        match size {
-            0 => continue,
-            1 => {
-                let index = index as u32;
-                index.push_to_lua(&mut lua).forget();
-                unsafe { hlua_ffi::lua_insert(lua.as_mut_lua().0, -2) }
-                unsafe { hlua_ffi::lua_settable(lua.as_mut_lua().0, -3) }
-            },
-            2 => unsafe { hlua_ffi::lua_settable(lua.as_mut_lua().0, -3) },
-            _ => unreachable!()
-        }
-    }
-
-    PushGuard { lua: lua, size: 1 }
-}
-
-
-fn push_rec_iter<L, V, I>(mut lua: L, iterator: I) -> PushGuard<L>
-    where L: AsMutLua, V: for<'a> Push<&'a mut L>, I: Iterator<Item=V>
-{
-    let (nrec, _) = iterator.size_hint();
-
-    // creating empty table with pre-allocated non-array elements
-    unsafe { hlua_ffi::lua_createtable(lua.as_mut_lua().0, 0, nrec as i32) };
-
-    for elem in iterator {
-        let size = elem.push_to_lua(&mut lua).forget();
-
-        match size {
-            0 => continue,
-            2 => unsafe { hlua_ffi::lua_settable(lua.as_mut_lua().0, -3) },
-            _ => unreachable!()
-        }
-    }
-
-    PushGuard { lua: lua, size: 1 }
-}
-
-fn push_table_field<L, V>(mut lua: L, name: String, val: V)
-    where L: AsMutLua, V: for<'a> Push<&'a mut L> {
-    name.push_to_lua(&mut lua);
-    val.push_to_lua(&mut lua);
-    unsafe { hlua_ffi::lua_settable(lua.as_mut_lua().0, -3); }
-}
-
-use std::collections::BTreeMap;
-
-impl<L: AsMutLua> Push<L> for JsonWrap {
-    fn push_to_lua(self, lua: L) -> PushGuard<L> {
-        unsafe {
-            match self.0 {
-                Json::I64(val) => {
-                    (val as i32).push_to_lua(lua)
-                },
-                Json::U64(val) => {
-                    (val as u32).push_to_lua(lua)
-                },
-                Json::F64(val) => {
-                    val.push_to_lua(lua)
-                },
-                Json::String(val) => {
-                    val.push_to_lua(lua)
-                },
-                Json::Null => {
-                    ().push_to_lua(lua)
-                },
-                Json::Boolean(val) => {
-                    val.push_to_lua(lua)
-                },
-                Json::Array(vals) => {
-                    push_iter(lua, vals.into_iter()
-                              .map(|j| JsonWrap(j)))
-                },
-                Json::Object(map_l) => {
-                    let map: BTreeMap<String, Json> = map_l;
-
-                    let len = map.len();
-
-                    unsafe {
-                        hlua_ffi::lua_newtable(lua.as_mut_lua().0);
-                    }
-
-                    for (key, value) in map.into_iter() {
-                        push_table_field(&mut lua, key, JsonWrap(value));
-                    }
-                    PushGuard { lua: lua, size: 1 }
-                }
-            }
-        }
+    pub fn get_json(&self) -> Arc<Json> {
+        self.object.clone()
     }
 }
