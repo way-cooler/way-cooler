bf72e9976645fc8de54c7de91143ee6687316d11
snirk <snirk.immington@gmail.com>
2016-04-14 19:10:33 -0400

    Fix registry compile errors

--- src/lua/funcs/lua_registry.rs
+++ src/lua/funcs/lua_registry.rs
@@ -35,7 +35,7 @@ pub fn index(table: AnyLuaValue, lua_key: AnyLuaValue)
 // Prevent lua from changing the registry?
 pub fn new_index(table: AnyLuaValue, key: AnyLuaValue, val: AnyLuaValue)
                  -> Result<(), &'static str> {
-    //Err("You can't set the registry!")
+    Err("You can't set the registry!")
 }
 
 fn convert_json(json: Json) -> AnyLuaValue {
