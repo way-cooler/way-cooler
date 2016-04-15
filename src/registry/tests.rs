661df5cb582599b2a1dc388baa938270b25e4c87
Snirk Immington <snirk.immington@gmail.com>
2016-04-15 11:32:54 -0400

    Fix tests and add 5-threads-reading test

--- src/registry/tests.rs
+++ src/registry/tests.rs
@@ -1,7 +1,11 @@
 //! Tests for the registry
 
 use std::collections::BTreeMap;
+use std::sync::mpsc;
+use std::thread;
+
 use super::*;
+
 use rustc_serialize::{Decodable, Decoder};
 use rustc_serialize::json;
 use rustc_serialize::json::{Json, ToJson};
@@ -20,8 +24,6 @@ impl ToJson for Point {
         Json::Object(map)
     }
 }
-
-const i32_SIZE: usize = 32usize / 8usize;
 
 impl Decodable for Point {
     fn decode<D: Decoder>(d: &mut D) -> Result<Self, D::Error> {
@@ -55,12 +57,55 @@ fn registry_tests() {
     assert!(contains_key(&String::from("test_numbers")));
     assert!(contains_key(&String::from("test_point")));
 
-    assert_eq!(get::<_, i32>(&String::from("test_num")).unwrap(), num);
-    assert_eq!(get::<_, f64>(&String::from("test_double")).unwrap(), double);
-    assert_eq!(get::<_, String>(&String::from("test_string")).unwrap(), string);
-    assert_eq!(get::<_, Vec<i32>>(&String::from("test_numbers")).unwrap(),
+    assert_eq!(get::<_, i32>(&String::from("test_num")).unwrap().1, num);
+    assert_eq!(get::<_, f64>(&String::from("test_double")).unwrap().1, double);
+    assert_eq!(get::<_, String>(&String::from("test_string")).unwrap().1, string);
+    assert_eq!(get::<_, Vec<i32>>(&String::from("test_numbers")).unwrap().1,
                numbers);
-    assert_eq!(get::<_, Point>(&String::from("test_point")).unwrap(), point);
+    assert_eq!(get::<_, Point>(&String::from("test_point")).unwrap().1, point);
 
+    let (tx, rx) = mpsc::channel();
 
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        read_thread(String::from("test_num"), num, tx1);
+    });
+    let tx2 = tx.clone();
+    thread::spawn(move || {
+        read_thread(String::from("test_double"), double, tx2);
+    });
+    let tx3 = tx.clone();
+    thread::spawn(move || {
+        read_thread(String::from("test_string"), string, tx3);
+    });
+    let tx4 = tx.clone();
+    thread::spawn(move || {
+        read_thread(String::from("test_numbers"), numbers, tx4);
+    });
+    let tx5 = tx.clone();
+    thread::spawn(move || {
+        read_thread(String::from("test_point"), point, tx5);
+    });
+
+    let mut result = true;
+
+    for i in 0..5 {
+        result = result && rx.recv().unwrap();
+    }
+    assert!(result);
+}
+
+fn read_thread<T>(name: String, in_val: T, sender: mpsc::Sender<bool>)
+where T: ::std::fmt::Debug + Decodable + PartialEq {
+    for _ in 1 .. 100 {
+        if let Ok(acc_val) = get::<_, T>(&name) {
+            let (acc, val) = acc_val;
+            assert_eq!(acc, RegistryAccess::Public);
+            assert_eq!(val, in_val);
+        }
+        else {
+            sender.send(false).unwrap();
+        }
+    }
+    sender.send(true).unwrap();
 }
