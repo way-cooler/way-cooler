5aca07bb55386828f857334b73e2112904016d31
Timidger <APragmaticPlace@gmail.com>
2016-04-25 16:56:41 -0400

    Added tests for root, including one that currently fails (tests if you can only make one root, haven't implemented a mechanism for that yet

--- src/layout/tests.rs
+++ src/layout/tests.rs
@@ -7,14 +7,54 @@ mod tests {
     use super::super::containers::{Container, ContainerType, Layout, Node};
 
     #[test]
-    fn it_works() {
-        let mut result = vec!();
-        for _ in 1..10 {
-            result.push(Container::new_root());
-        }
-        for thing in result {
-            drop(thing);
-        }
-        //assert_eq!(result[
+    #[should_panic(expected = "Can only be one root")]
+    /// Test to make sure that there can be only one root node
+    fn test_one_root() {
+        let root = Container::new_root();
+        // Should panic
+        let root2 = Container::new_root();
+    }
+
+    #[test]
+    /// Verify that root node has required properties
+    fn root_validity() {
+        let root = Rc::new(Container::new_root());
+        // Make sure it starts with no children
+        // This will change when we add Workspaces when we make the root
+        assert!(root.get_children().is_none());
+        // This is the tippity-top of the tree
+        assert!(root.get_parent().is_none()); 
+        assert_eq!(root.get_type(), ContainerType::Root);
+        // Should this be true? It makes sense, but it might cause complications
+        // so we'll go with false for now
+        assert!(! root.is_focused());
+        // Root is parent of all nodes, including itself
+        assert!(root.is_parent_of(root.clone()));
+        // Root is not a child of anything, including itself
+        // NOTE Add more test when we have more container types
+        assert!(! root.is_child_of(root.clone()));
+        assert!(root.is_root());
+    }
+
+    #[test]
+    #[should_panic(expected = "Cannot remove the root of the tree")]
+    /// Ensures you cannot remove the root node
+    fn remove_root_test() {
+        let root = Container::new_root();
+        root.remove_container();
+    }
+
+    #[test]
+    #[should_panic(expected = "Root has no dimensions")]
+    fn get_root_dimensions_test() {
+        let root = Container::new_root();
+        root.get_dimensions();
+    }
+    
+    #[test]
+    #[should_panic(expected = "Root has no position")]
+    fn get_root_position_test() {
+        let root = Container::new_root();
+        root.get_position();
     }
 }
