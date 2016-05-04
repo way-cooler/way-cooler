3627f55512b0018993ee4f8e61c9691f6f59cd5d
snirk <snirk.immington@gmail.com>
2016-05-04 02:41:29 -0400

    Add constructor methods for Container

--- src/layout/container.rs
+++ src/layout/container.rs
@@ -77,6 +77,46 @@ pub enum Container {
 }
 
 impl Container {
+    /// Creates a new root container
+    pub fn new_root() -> Container {
+        Container::Root
+    }
+
+    /// Creates a new output container with the given output
+    pub fn new_output(handle: WlcOutput) -> Container {
+        Container::Output {
+            handle: handle
+        }
+    }
+
+    /// Creates a new workspace container with the given name
+    pub fn new_workspace(name: String) -> Container {
+        Container::Workspace {
+            name: name, focused: false
+        }
+    }
+
+    /// Creates a new container
+    pub fn new_container() -> Container {
+        Container::Container {
+            layout: Layout::Horizontal, // default layout?
+            visible: false,
+            focused: false,
+            floating: false
+        }
+    }
+
+    /// Creates a new view container with the given handle
+    pub fn new_view(handle: WlcView) -> Container {
+        Container::View {
+            handle: handle,
+            visible: false,
+            focused: false,
+            floating: false
+        }
+    }
+
+    /// Gets the type of this container
     pub fn get_type(&self) -> ContainerType {
         match *self {
             Container::Root => ContainerType::Root,
