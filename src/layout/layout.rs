629f388683695998b55b8e2cd9b8b924976c9e74
Timidger <APragmaticPlace@gmail.com>
2016-05-06 00:17:03 -0400

    RootNode -> Tree

--- src/layout/layout.rs
+++ src/layout/layout.rs
@@ -10,12 +10,12 @@ pub mod layout {
 
     use std::ptr;
 
-    struct RootNode {
+    struct Tree {
         root: Node,
         active_container: *const Node,
     }
 
-    impl RootNode {
+    impl Tree {
         fn get_active_container(&self) -> Option<&Node> {
             if self.active_container.is_null() {
                 None
@@ -27,11 +27,11 @@ pub mod layout {
         }
     }
 
-    unsafe impl Send for RootNode {}
+    unsafe impl Send for Tree {}
 
     lazy_static! {
-        static ref ROOT: Mutex<RootNode> = {
-            Mutex::new(RootNode{
+        static ref TREE: Mutex<Tree> = {
+            Mutex::new(Tree{
                 root: Node::new(Container::new_root()),
                 active_container: ptr::null(),
             })
@@ -39,7 +39,7 @@ pub mod layout {
     }
 
     pub fn add_output(wlc_output: WlcOutput) {
-        let mut root = ROOT.lock().unwrap();
+        let mut root = TREE.lock().unwrap();
         let output = Container::new_output(wlc_output);
         root.root.new_child(output);
         drop(root);
@@ -49,14 +49,14 @@ pub mod layout {
 
     pub fn add_workspace(name: &str) {
         trace!("Adding new workspace to root");
-        let mut root = ROOT.lock().unwrap();
+        let mut root = TREE.lock().unwrap();
         let workspace = Container::new_workspace(name.to_string());
         // NOTE handle multiple outputs
         root.root.get_children_mut()[0].new_child(workspace);
     }
 
     pub fn add_view(wlc_view: WlcView) {
-        let mut root = ROOT.lock().unwrap();
+        let mut root = TREE.lock().unwrap();
         if let Some(current_workspace) = get_current_workspace(&root) {
             trace!("Adding view {:?} to {:?}", wlc_view, current_workspace);
             current_workspace.new_child(Container::new_view(wlc_view));
@@ -64,7 +64,7 @@ pub mod layout {
     }
 
     pub fn remove_view(wlc_view: &WlcView) {
-        let mut root = ROOT.lock().unwrap();
+        let mut root = TREE.lock().unwrap();
         if let Some(view) = root.root.find_view_by_handle(&wlc_view) {
             let parent = view.get_parent().unwrap();
             parent.remove_child(view);
@@ -73,7 +73,7 @@ pub mod layout {
 
     pub fn switch_workspace(name: &str) {
         trace!("Switching to workspace {}", name);
-        let mut root = ROOT.lock().unwrap();
+        let mut root = TREE.lock().unwrap();
         if let Some(old_workspace) = get_current_workspace(&root) {
             // Make all the views in the original workspace to be invisible
             for view in old_workspace.get_children_mut() {
@@ -93,7 +93,7 @@ pub mod layout {
             } else {
                 drop(root);
                 add_workspace(name);
-                root = ROOT.lock().unwrap();
+                root = TREE.lock().unwrap();
                 new_current_workspace = get_workspace_by_name_mut(&mut root, name).unwrap();
             }
             for view in new_current_workspace.get_children_mut() {
@@ -118,7 +118,7 @@ pub mod layout {
         root.active_container = current_workspace;
     }
 
-    fn get_focused_workspace<'a>(root: &'a RootNode) -> Option<&'a Node> {
+    fn get_focused_workspace<'a>(root: &'a Tree) -> Option<&'a Node> {
         for output in root.root.get_children() {
             if output.get_val().is_focused() {
                 for workspace in output.get_children() {
@@ -132,7 +132,7 @@ pub mod layout {
 
     }
 
-    fn get_current_workspace<'a>(root: &'a RootNode) -> Option<&'a mut Node> {
+    fn get_current_workspace<'a>(root: &'a Tree) -> Option<&'a mut Node> {
         if let Some(container) = root.get_active_container() {
             //if let Some(child) = container.get_ancestor_of_type(ContainerType::Workspace) {
                 //return child.get_children()[0].get_parent()
@@ -149,7 +149,7 @@ pub mod layout {
         return None
     }
 
-    fn get_workspace_by_name<'a, 'b>(root: &'a RootNode, name: &'b str) -> Option<&'a Node> {
+    fn get_workspace_by_name<'a, 'b>(root: &'a Tree, name: &'b str) -> Option<&'a Node> {
         for child in root.root.get_children()[0].get_children() {
             if child.get_val().get_name().unwrap() != name {
                 continue
@@ -159,7 +159,7 @@ pub mod layout {
         return None
     }
 
-    fn get_workspace_by_name_mut<'a, 'b>(root: &'a mut RootNode, name: &'b str) -> Option<&'a mut Node> {
+    fn get_workspace_by_name_mut<'a, 'b>(root: &'a mut Tree, name: &'b str) -> Option<&'a mut Node> {
         for child in root.root.get_children_mut()[0].get_children_mut() {
             if child.get_val().get_name().unwrap() != name {
                 continue
