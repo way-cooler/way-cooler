a01e880adc9964f838f1b58e4241525aef314984
Snirk Immington <snirk.immington@gmail.com>
2016-05-16 22:33:56 -0400

    Registry values are Json or commands

--- src/registry/types.rs
+++ src/registry/types.rs
@@ -1,51 +1,100 @@
 //! Types used in the registry.
 
 use std::sync::Arc;
+use std::fmt::{Debug, Formatter};
+use std::fmt::Result as FmtResult;
 
-use hlua::any::AnyLuaValue;
-
-use super::super::convert::ToTable;
+use rustc_serialize::json::{Json, ToJson};
 
 bitflags! {
     /// Access permissions for items in the registry
     pub flags AccessFlags: u8 {
         #[allow(dead_code)]
-        const LUA_PRIVATE = 0b00000000,
-        const LUA_READ    = 0b00000001,
-        #[allow(dead_code)]
-        const LUA_WRITE   = 0b00000010,
+        /// Default flags
+        const LUA_PRIVATE = 0,
+        /// Lua thread can read the data
+        const LUA_READ    = 1 << 0,
+        /// Lua thread can write to the data
+        const LUA_WRITE   = 1 << 1,
     }
 }
 
-/// Values stored in the registry
-#[derive(Debug, PartialEq)]
-pub struct RegistryValue {
-    flags: AccessFlags,
-    object: Arc<AnyLuaValue>
+/// Command type for Rust function
+pub type CommandFn = Arc<Fn() + Send + Sync>;
+
+/// Data which can be stored in the registry
+#[derive(Clone)]
+pub enum RegistryValue {
+    /// An object with permission flags
+    Object {
+        /// Permission flags for Lua get/setting the value
+        flags: AccessFlags,
+        /// Data associated with this value
+        data: Arc<Json>
+    },
+    /// A command
+    Command(CommandFn)
+}
+
+impl Debug for RegistryValue {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        match self {
+            &RegistryValue::Object { ref flags, ref data } =>
+                f.debug_struct("RegistryValue::Object")
+                .field("flags", flags as &Debug)
+                .field("data", data as &Debug).finish(),
+            &RegistryValue::Command(_) =>
+                write!(f, "RegistryValue::Command(...)")
+        }
+    }
 }
 
 impl RegistryValue {
-    /// Creates a new RegistryValue
+    /// Creates a new registry object with the specified flags
+    /// and Rust data to be converted.
     #[allow(dead_code)]
-    pub fn new<T>(flags: AccessFlags, data: T) -> RegistryValue
-        where T: ToTable  {
-        RegistryValue {
+    pub fn new_value<T: ToJson>(flags: AccessFlags, data: T) -> RegistryValue {
+        RegistryValue::Object {
             flags: flags,
-            object: Arc::new(data.to_table())
+            data: Arc::new(data.to_json())
         }
     }
 
+    /// Creates a new RegistryFile with the specified Lua data.
+    pub fn new_json(flags: AccessFlags, data: Json) -> RegistryValue {
+        RegistryValue::Object {
+            flags: flags, data: Arc::new(data)
+        }
+    }
+
+    /// Creates a new RegistryCommand with the specified callback.
+    pub fn new_command(com: CommandFn) -> RegistryValue {
+        RegistryValue::Command(com)
+    }
+
     /// What access the module has to it
-    pub fn flags(&self) -> AccessFlags {
-        self.flags
+    #[allow(dead_code)]
+    pub fn get_flags(&self) -> Option<AccessFlags> {
+        match self {
+            &RegistryValue::Object { ref flags, .. } => Some(flags.clone()),
+            _ => None
+        }
     }
 
-    /// Gets the Lua of a registry value
-    pub fn get_lua(&self) -> Arc<AnyLuaValue> {
-        self.object.clone()
+    /// Attempts to access the RegistryValue as a command
+    pub fn get_command(self) -> Option<CommandFn> {
+        match self {
+            RegistryValue::Command(com) => Some(com),
+            _ => None
+        }
     }
 
-    pub fn set_lua(&mut self, lua: AnyLuaValue) {
-        self.object = Arc::new(lua);
+    /// Attempts to access the RegistryValue as a file
+    pub fn get_data(&self) -> Option<(AccessFlags, Arc<Json>)> {
+        match *self {
+            RegistryValue::Object { ref flags, ref data } =>
+                Some((flags.clone(), data.clone())),
+            _ => None
+        }
     }
 }
