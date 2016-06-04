8fa602b83ccd38379b0a1dba67a31c4537eef0f3
Snirk Immington <snirk.immington@gmail.com>
2016-06-04 11:26:54 -0400

    Add tests for panicking properties, currently poison the registry

--- src/registry/tests.rs
+++ src/registry/tests.rs
@@ -32,13 +32,24 @@ pub fn get_prop() -> Json {
     PROP_GET_RESULT.to_json()
 }
 
+pub fn get_panic_prop() -> Json {
+    panic!("get_panic_prop panic")
+}
+
 pub fn set_prop(_json: Json) {
     println!("set_prop being called!");
-    panic!("set_prop panic")
+}
+
+pub fn set_panic_prop(_json: Json) {
+    panic!("set_panic_prop panic")
 }
 
 pub fn command() {
-    panic!("command panic");
+    println!("command being run!");
+}
+
+pub fn panic_command() {
+    panic!("panic_command panic")
 }
 
 /// [0, 1, 2]
@@ -71,18 +82,18 @@ unsafe impl Sync for Point {}
 #[test]
 fn add_keys() {
     let values = vec![
-        ("bool", AccessFlags::all(), BOOL.to_json()),
-        ("u64", AccessFlags::all(), U64.to_json()),
-        ("i64", AccessFlags::all(), I64.to_json()),
-        ("f64", AccessFlags::all(), F64.to_json()),
-        ("text", AccessFlags::all(), TEXT.to_json()),
+        ("bool",  AccessFlags::all(), BOOL.to_json()),
+        ("u64",   AccessFlags::all(), U64.to_json()),
+        ("i64",   AccessFlags::all(), I64.to_json()),
+        ("f64",   AccessFlags::all(), F64.to_json()),
+        ("text",  AccessFlags::all(), TEXT.to_json()),
         ("point", AccessFlags::all(), POINT.to_json()),
-        ("null", AccessFlags::all(), Json::Null),
-        ("u64s", AccessFlags::all(), Json::Array(u64s())),
+        ("null",  AccessFlags::all(), Json::Null),
+        ("u64s",  AccessFlags::all(), Json::Array(u64s())),
 
-        ("readonly", AccessFlags::READ(), READONLY.to_json()),
+        ("readonly",  AccessFlags::READ(),  READONLY.to_json()),
         ("writeonly", AccessFlags::WRITE(), WRITEONLY.to_json()),
-        ("noperms", AccessFlags::empty(), NO_PERMS.to_json())
+        ("noperms",   AccessFlags::empty(), NO_PERMS.to_json())
     ];
 
     for (name, flags, json) in values.into_iter() {
@@ -90,21 +101,38 @@ fn add_keys() {
             .expect("Unable to initialize objects in registry");
     }
 
+    // Command
     registry::set_command("command".to_string(), Arc::new(command))
         .expect("Unable to initialize command in registry");
 
+    // Panicking command
+    registry::set_command("panic_command".to_string(), Arc::new(panic_command))
+        .expect("Unable to initialize command in registry");
+
+    // Read/write property
     registry::set_property_field("prop".to_string(),
                                  Some(Arc::new(get_prop)),
                                  Some(Arc::new(set_prop)))
         .expect("Unable to initialize property in registry");
 
-    registry::set_property_field("prop_read".to_string(),
+    // Readonly/writeonly properties
+    registry::set_property_field("get_prop".to_string(),
                                  Some(Arc::new(get_prop)),
                                  None)
         .expect("Unable to initialize property in registry");
-    registry::set_property_field("prop_write".to_string(),
+    registry::set_property_field("set_prop".to_string(),
                                  None,
                                  Some(Arc::new(set_prop)))
+        .expect("Unable to initialize property in registry");
+
+    // Readonly/writeonly panicking properties
+    registry::set_property_field("get_panic_prop".to_string(),
+                                 Some(Arc::new(get_panic_prop)),
+                                 None)
+        .expect("Unable to initialize property in registry");
+    registry::set_property_field("set_panic_prop".to_string(),
+                                 None,
+                                 Some(Arc::new(set_panic_prop)))
         .expect("Unable to initialize property in registry");
 
     // Allow waiting threads to continue
@@ -121,7 +149,8 @@ fn contains_keys() {
     let keys = [
         "bool", "u64", "i64", "f64", "null", "text", "point",
         "u64s", "readonly", "writeonly", "prop", "noperms",
-        "prop_read", "prop_write", "command",
+        "get_prop", "set_prop", "command",
+        "get_panic_prop", "set_panic_prop", "panic_command",
     ];
     for key in keys.into_iter() {
         assert!(registry::contains_key(key),
@@ -130,22 +159,22 @@ fn contains_keys() {
 }
 
 #[test]
-fn object_keys_equal() {
+fn objects_and_keys_equal() {
     wait_for_registry();
 
     let values = vec![
-        ("bool", AccessFlags::all(), BOOL.to_json()),
-        ("u64", AccessFlags::all(), U64.to_json()),
-        ("i64", AccessFlags::all(), I64.to_json()),
-        ("f64", AccessFlags::all(), F64.to_json()),
-        ("text", AccessFlags::all(), TEXT.to_json()),
+        ("bool",  AccessFlags::all(), BOOL.to_json()),
+        ("u64",   AccessFlags::all(), U64.to_json()),
+        ("i64",   AccessFlags::all(), I64.to_json()),
+        ("f64",   AccessFlags::all(), F64.to_json()),
+        ("text",  AccessFlags::all(), TEXT.to_json()),
         ("point", AccessFlags::all(), POINT.to_json()),
-        ("null", AccessFlags::all(), Json::Null),
-        ("u64s", AccessFlags::all(), Json::Array(u64s())),
+        ("null",  AccessFlags::all(), Json::Null),
+        ("u64s",  AccessFlags::all(), Json::Array(u64s())),
 
-        ("readonly", AccessFlags::READ(), READONLY.to_json()),
+        ("readonly",  AccessFlags::READ(),  READONLY.to_json()),
         ("writeonly", AccessFlags::WRITE(), WRITEONLY.to_json()),
-        ("noperms", AccessFlags::empty(), NO_PERMS.to_json())
+        ("noperms",   AccessFlags::empty(), NO_PERMS.to_json())
     ];
 
     for (name, flags, json) in values.into_iter() {
@@ -161,10 +190,10 @@ fn key_perms() {
     wait_for_registry();
 
     let perms = vec![
-        ("bool", AccessFlags::all()),
-        ("readonly", AccessFlags::READ()),
+        ("bool",      AccessFlags::all()),
+        ("readonly",  AccessFlags::READ()),
         ("writeonly", AccessFlags::WRITE()),
-        ("prop", AccessFlags::all()),
+        ("prop",      AccessFlags::all()),
         //("prop_read", AccessFlags::READ()),
         //("prop_write", AccessFlags::WRITE())
     ];
@@ -180,7 +209,6 @@ fn key_perms() {
 }
 
 #[test]
-#[should_panic(expected = "command panic")]
 fn run_command() {
     wait_for_registry();
     let command = registry::get_command("command")
@@ -189,22 +217,46 @@ fn run_command() {
 }
 
 #[test]
+#[should_panic(expected = "command panic")]
+fn run_panic_command() {
+    wait_for_registry();
+    let command = registry::get_command("panic_command")
+        .expect("Command not found");
+    command();
+}
+
+#[test]
 fn property_get() {
     wait_for_registry();
-    let prop_read = registry::get_data("prop_read")
+    let prop_read = registry::get_data("get_prop")
         .expect("Couldn't get prop_read");
 
     assert_eq!(*prop_read.resolve().1, PROP_GET_RESULT.to_json());
 }
 
 #[test]
-//#[should_panic(expected = "set_prop panic")]
+#[should_panic(expected="get_panic_prop panic")]
+fn panicking_property_get() {
+    wait_for_registry();
+    let prop_read = registry::get_data("get_panic_prop")
+        .expect("Couldn't get prop_read");
+
+    assert_eq!(*prop_read.resolve().1, PROP_GET_RESULT.to_json());
+}
+
+#[test]
 fn property_set() {
     wait_for_registry();
     assert!(registry::set_json("set_prop".to_string(),
                                AccessFlags::empty(), Json::Null)
             .expect("Unable to set data").is_none());
-    registry::set_json("set_prop".to_string(),
-                       AccessFlags::empty(), Json::Null)
-        .expect("Couldn't set data a second time");
+}
+
+#[test]
+#[should_panic(expected="set_panic_prop panic")]
+fn panicking_property_set() {
+    wait_for_registry();
+    assert!(registry::set_json("set_panic_prop".to_string(),
+                               AccessFlags::empty(), Json::Null)
+            .expect("Unable to set data").is_none());
 }
