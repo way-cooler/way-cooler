2a0e07dd6b47e0461fae316ff1279e3c59565fc9
Timidger <APragmaticPlace@gmail.com>
2016-06-20 17:36:27 -0400

    Revert "Removed tree debug key"
    This reverts commit 190f9c08a3c604d686c1cd9c968df1c28f04f39e.

--- src/keys.rs
+++ src/keys.rs
@@ -90,7 +90,8 @@ lazy_static! {
             move_left, keypress!("Alt", "left");
             move_right, keypress!("Alt", "right");
             move_up, keypress!("Alt", "up");
-            move_down, keypress!("Alt", "down")
+            move_down, keypress!("Alt", "down");
+            debug_tree, keypress!("Alt", "q")
         }
 
         RwLock::new(map)
@@ -185,6 +186,12 @@ fn pointer_fn() {
 fn quit_fn() {
     info!("handler: Esc keypress!");
     ::rustwlc::terminate();
+}
+
+fn debug_tree() {
+    if let Ok(tree) = tree::try_lock_tree() {
+        error!("The tree: {:#?}", *tree);
+    }
 }
 
 #[derive(Eq, PartialEq, Clone, Debug)]
