95758f3df63bc4962e7a8ee4a48bf9f4359c696c
Snirk Immington <snirk.immington@gmail.com>
2016-06-28 21:26:10 -0400

    Remove extra json functions

--- src/ipc/tests/mod.rs
+++ src/ipc/tests/mod.rs
@@ -9,20 +9,3 @@ mod commands;
 
 // Integration tests for running the command socket
 mod command_loop;
-
-/// Parse text into Json
-#[macro_export]
-macro_rules! json {
-    ($text:expr) => {
-        match Json::from_str($text) {
-            Ok(json) => json,
-            Err(err) =>
-                panic!(format!("json! failed with input {} - {:?}", $text, err))
-        }
-    }
-}
-
-#[inline]
-pub fn json_eq(json: Json, text: &str) -> bool {
-    json == json!(text)
-}
