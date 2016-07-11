c764164f2df122eb98de81dff11400445334df8d
Snirk Immington <snirk.immington@gmail.com>
2016-07-10 23:03:02 -0400

    Update lua config library

--- lib/lua/config.lua
+++ lib/lua/config.lua
@@ -15,6 +15,17 @@ end
 
 -- Initialize the workspaces
 config.init_workspaces = function(count, settings)
+    error("This feature is not yet implemented!", 2) -- TODO implement
+    assert(type(count) == 'number', "count: expected number")
+    assert(type(settings) == 'table', "settings: expected table")
+    for ix, val in pairs(settings) do
+        assert(type(ix) == 'number', "settings: expected number-indexed array")
+        assert(type(val) == 'table', "settings: expected array of tables")
+
+        val.name = val.name or ""
+        val.mode = val.mode or "tiling"
+    end
+    rust.init_workspaces(count, settings)
 end
 
 -- Create a new keybinding to register with Rust
@@ -32,6 +43,19 @@ end
 
 local use_key = ", use the `key` or `config.key` method to create a keybinding"
 
+-- Converts a list of modifiers to a string
+local function keymods_to_string(mods, key)
+    table.sort(mods)
+    table.insert(mods, key)
+    return table.concat(mods, ',')
+end
+
+-- Save the action at the __key_map and tell Rust to register the Lua key
+local function register_lua_key(index, action, loop)
+    __key_map[index] = action
+    rust.register_lua_key(index, action, loop)
+end
+
 -- Register a keybinding
 config.register_key = function(key)
     assert(key.mods, "keybinding missing modifiers" .. use_key)
@@ -46,9 +70,11 @@ config.register_key = function(key)
            "keybinding repeat: expected optional boolean" .. use_key)
 
     if (type(key.action) == 'string') then
-        rust.register_command_key(key.mods, key.key, key.action, key.loop)
+        rust.register_command_key(keymods_to_string(key.mods),
+                                  key.key, key.action, key.loop)
     elseif (type(key.action) == 'function') then
-        rust.register_lua_key(key.mods, key.key, key.action, key.loop)
+        register_lua_key(rust.keymods_index(keymods_to_string(key.mods, key.key)),
+                              key.action, key.loop)
     else
         error("keybinding action: expected string or a function"..use_key, 2)
     end
