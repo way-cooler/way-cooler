15169fd5115b7a7fec0dd558a1951185df1df756
Timidger <APragmaticPlace@gmail.com>
2016-12-08 23:27:02 -0500

    Fixed root container test

--- src/layout/actions/layout.rs
+++ src/layout/actions/layout.rs
@@ -234,7 +234,7 @@ impl LayoutTree {
     /// it will remain pointing at the previous parent container.
     pub fn float_container(&mut self, id: Uuid) -> CommandResult {
         let node_ix = try!(self.tree.lookup_id(id).ok_or(TreeError::NodeNotFound(id)));
-        if self.root_container_ix() == Some(node_ix) {
+        if self.tree.is_root_container(node_ix) {
             return Err(TreeError::InvalidOperationOnRootContainer(id))
         }
         if self.tree[node_ix].floating() {
