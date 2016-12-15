7c125bbf6258f1445d805f44c8792e44b4f3c521
Timidger <APragmaticPlace@gmail.com>
2016-12-14 21:42:10 -0500

    Fixed mod

--- src/layout/core/border/mod.rs
+++ src/layout/core/border/mod.rs
@@ -1,8 +1,6 @@
 mod border;
-mod borders;
-mod new_border;
 
 
-pub use self::border::{Border, TopBorder, RightBorder, LeftBorder, BottomBorder, Color};
-pub use self::borders::Borders;
+//pub use self::border::{Border, TopBorder, RightBorder, LeftBorder, BottomBorder, Color};
+//pub use self::borders::Borders;
 
