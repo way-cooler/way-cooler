f17e423a7942b98df8afb8c7834f7448bef484e9
Timidger <APragmaticPlace@gmail.com>
2017-01-01 19:33:26 -0500

    Moved grabbing at point to pointer.rs

--- src/layout/actions/pointer.rs
+++ src/layout/actions/pointer.rs
@@ -1,4 +1,5 @@
-use rustwlc::{input, Point, ResizeEdge, Geometry};
+use rustwlc::{input, Point, ResizeEdge, Geometry,
+              RESIZE_TOPLEFT, RESIZE_TOPRIGHT, RESIZE_BOTTOMLEFT, RESIZE_BOTTOMRIGHT,};
 
 use super::super::{LayoutTree, TreeError};
 use super::super::commands::{CommandResult};
@@ -9,5 +10,27 @@ impl LayoutTree {
     pub fn set_pointer_pos(&mut self, point: Point) -> CommandResult {
         input::pointer::set_position(point);
         Ok(())
+    }
+
+    pub fn grab_at_corner(&mut self, id: Uuid, edge: ResizeEdge)
+                          -> Result<Point, TreeError> {
+        let container = try!(self.lookup(id));
+        let Geometry { mut origin, size } = container.get_geometry()
+            .expect("Container had no geometry");
+        drop(container);
+        if edge.contains(RESIZE_TOPLEFT) {
+            input::pointer::set_position(origin);
+        } else if edge.contains(RESIZE_TOPRIGHT) {
+            origin.x += size.w as i32;
+            input::pointer::set_position(origin);
+        } else if edge.contains(RESIZE_BOTTOMLEFT) {
+            origin.y += size.h as i32;
+            input::pointer::set_position(origin);
+        } else if edge.contains(RESIZE_BOTTOMRIGHT) {
+            origin.x += size.w as i32;
+            origin.y += size.h as i32;
+            input::pointer::set_position(origin);
+        }
+        Ok((origin))
     }
 }
