e89ab442f5a1b9829645b6a2d013d91e987b18ef
Timidger <APragmaticPlace@gmail.com>
2017-01-05 23:34:00 -0500

    Added debugging line when adding a child node

--- src/layout/core/graph_tree.rs
+++ src/layout/core/graph_tree.rs
@@ -187,6 +187,7 @@ impl InnerTree {
         if let Some(view) = maybe_view {
             self.view_map.insert(view, child_ix);
         }
+        debug!("Added new child {:?} for {:?}", child_ix, &self[child_ix]);
         child_ix
     }
 
