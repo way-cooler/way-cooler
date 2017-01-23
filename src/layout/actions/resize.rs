fc45565e3702f0fe6e096ea1be22799075c2b2e8
Timidger <APragmaticPlace@gmail.com>
2017-01-22 20:35:39 -0500

    Resize floating uses actual geometry of container

--- src/layout/actions/resize.rs
+++ src/layout/actions/resize.rs
@@ -28,7 +28,7 @@ impl LayoutTree {
                 _ => return Err(TreeError::UuidWrongType(container.get_id(),
                                                         vec!(ContainerType::View)))
             }
-            let geo = container.get_geometry()
+            let geo = container.get_actual_geometry()
                 .expect("Could not get geometry of the container");
 
             let new_geo = calculate_resize(geo, edge, pointer, action.grab);
