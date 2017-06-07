53da5876f7a2fc02a830cc28dc9207c35a396cb7
Timidger <APragmaticPlace@gmail.com>
2017-06-06 21:23:35 -0500

    Fixed function names

--- src/layout/unit_tests/tests.rs
+++ src/layout/unit_tests/tests.rs
@@ -5,8 +5,8 @@ fn ensure_workspace_switch_when_fullscreen() {
     let mut tree = basic_tree();
     let active_id = tree.active_id().unwrap();
     tree.set_fullscreen(active_id, true).unwrap();
-    tree = tree.add_view().unwrap();
-    tree = tree.switch_to_workspace("2").unwrap();
-    tree = tree.switch_to_workspace("1").unwrap();
+    tree = tree.add_new_view().unwrap();
+    tree = tree.add_workspace("2").unwrap();
+    tree = tree.add_workspace("1").unwrap();
     assert_eq!(tree.active_id(), Some(active_id));
 }
