67a54981def1162164bb741565f7c14a104e65df
Timidger <APragmaticPlace@gmail.com>
2017-09-01 19:16:24 -0700

    Added AwesomeWM mousegrabber API

--- src/awesome/mod.rs
+++ src/awesome/mod.rs
@@ -1,12 +1,15 @@
 //! Awesome compatibilty modules
 use rlua::{self, Lua};
 pub mod keygrabber;
+pub mod mousegrabber;
 pub mod awful;
 
 pub use self::keygrabber::keygrabber_handle;
+pub use self::mousegrabber::mousegrabber_handle;
 
 pub fn init(lua: &Lua) -> rlua::Result<()> {
     keygrabber::init(lua)?;
+    mousegrabber::init(lua)?;
     awful::init(lua)?;
     Ok(())
 }
