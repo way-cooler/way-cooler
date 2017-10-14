62822e89a760e06e64366a652ec9015c6dbd02b0
Timidger <APragmaticPlace@gmail.com>
2017-10-14 11:51:53 -0700

    Fixed bg program to be wc-bg (from way-cooler-bg)

--- config/init.lua
+++ config/init.lua
@@ -24,7 +24,7 @@ way_cooler.programs = {
 
 -- Registering programs to run at startup
 -- These programs are only ran once util.program.spawn_programs is called.
-util.program.spawn_at_startup("way-cooler-bg")
+util.program.spawn_at_startup("wc-bg")
 
 -- These options are applied to all windows.
 way_cooler.windows = {
