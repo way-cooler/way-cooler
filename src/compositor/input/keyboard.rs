aa88910b9d3714df76238f86df7d6895bb30c777
Timidger <APragmaticPlace@gmail.com>
2018-04-05 18:16:02 -0400

    Spawn, focus, and type into a weston-terminal
    Debug is f1, will be removed later.
    
    Next is to be able to resize and move it around.

--- src/compositor/input/keyboard.rs
+++ src/compositor/input/keyboard.rs
@@ -1,15 +1,30 @@
-use wlroots::{self, Compositor, KeyboardHandler, key_events::KeyEvent, xkbcommon::xkb::KEY_Escape};
+use compositor::Server;
+use wlroots::{self, Compositor, KeyboardHandler, key_events::KeyEvent,
+              xkbcommon::xkb::{KEY_Escape, KEY_F1}, WLR_KEY_PRESSED};
 pub struct Keyboard;
 
 impl KeyboardHandler for Keyboard {
     fn on_key(&mut self,
               compositor: &mut Compositor,
-              _: &mut wlroots::Keyboard,
+              keyboard: &mut wlroots::Keyboard,
               event: &mut KeyEvent) {
-        for key in event.pressed_keys() {
-            if key == KEY_Escape {
-                compositor.terminate()
+        if event.key_state() == WLR_KEY_PRESSED {
+            for key in event.pressed_keys() {
+                if key == KEY_Escape {
+                    compositor.terminate()
+                // TODO Remove
+                } else if key == KEY_F1 {
+                    ::std::thread::spawn(|| {
+                                             ::std::process::Command::new("weston-terminal").output()
+                                                                       .unwrap()
+                                         });
+                }
             }
         }
+        let server: &mut Server = compositor.into();
+        run_handles!([(seat: {&mut server.seat})] => {
+            seat.keyboard_notify_key(event.time_msec(), event.keycode(), event.key_state() as u32);
+            seat.keyboard_send_modifiers(&mut keyboard.get_modifier_masks())
+        }).expect("Seat was destroyed");
     }
 }
--- src/compositor/input/pointer.rs
+++ src/compositor/input/pointer.rs
@@ -1,5 +1,5 @@
-use compositor::{self, Server};
-use wlroots::{self, Compositor, PointerHandler, pointer_events::*};
+use compositor::{self, Server, Shell};
+use wlroots::{self, Area, Compositor, Origin, PointerHandler, Size, pointer_events::*};
 
 #[derive(Debug, Default)]
 pub struct Pointer;
@@ -16,4 +16,56 @@ impl PointerHandler for Pointer {
             cursor.move_to(event.device(), x, y);
         }).expect("Cursor was destroyed");
     }
+
+    fn on_button(&mut self,
+                 compositor: &mut Compositor,
+                 _: &mut wlroots::Pointer,
+                 _: &ButtonEvent) {
+        let server: &mut Server = compositor.into();
+        shell_at(server);
+    }
+}
+
+fn shell_at(server: &mut Server) -> Option<Shell> {
+    let Server { ref mut cursor,
+                 ref mut shells,
+                 ref mut seat,
+                 ref mut keyboards,
+                 .. } = *server;
+    for shell in shells {
+        match *shell {
+            Shell::XdgV6(ref mut shell) => {
+                let (mut sx, mut sy) = (0.0, 0.0);
+                let seen = run_handles!([(shell: {&mut *shell}),
+                                         (cursor: {&mut *cursor})] => {
+                    let (lx, ly) = cursor.coords();
+                    let Origin {x: shell_x, y: shell_y} = shell.geometry().origin;
+                    let (view_sx, view_sy) = (lx - shell_x as f64, ly - shell_y as f64);
+                    shell.surface_at(view_sx, view_sy, &mut sx, &mut sy).is_some()
+                }).ok()?.ok()?;
+                // TODO Use those surface level coordinates to send events and shit
+                if seen {
+                    for keyboard in { &mut *keyboards } {
+                        run_handles!([(seat: {&mut *seat}),
+                                      (shell: {&mut *shell}),
+                                      (surface: {shell.surface()}),
+                                      (keyboard: {keyboard})] => {
+                            use wlroots::XdgV6ShellState::*;
+                            match shell.state() {
+                                Some(&mut TopLevel(ref mut toplevel)) => {
+                                    // TODO Don't send this for each keyboard!
+                                    toplevel.set_activated(true);
+                                },
+                                _ => unimplemented!()
+                            }
+                            seat.keyboard_notify_enter(surface,
+                                                       &mut keyboard.keycodes(),
+                                                       &mut keyboard.get_modifier_masks())
+                        }).ok()?.ok()?.ok()?.ok()?;
+                    }
+                }
+            }
+        }
+    }
+    None
 }
