2234421edbd903125faa768a239537a42e347167
Timidger <APragmaticPlace@gmail.com>
2019-06-20 08:21:46 -0400

    Revert "Fix incorrect formatting of pointer declarations"
    This reverts commit e46a09eada9778f1d902cb127e45da83863a440b.

--- compositor/output.c
+++ compositor/output.c
@@ -19,23 +19,23 @@
 /* Used to move all of the data necessary to render a surface from the top-level
  * frame handler to the per-surface render function. */
 struct wc_view_render_data {
-	struct wlr_output *output;
-	struct wlr_renderer *renderer;
-	struct wc_view *view;
-	struct timespec *when;
+	struct wlr_output* output;
+	struct wlr_renderer* renderer;
+	struct wc_view* view;
+	struct timespec* when;
 };
 
 /* Used to move all of the data necessary to render a surface from the layers */
 struct wc_layer_render_data {
-	struct wlr_renderer *renderer;
-	struct wc_layer *layer;
-	struct timespec *when;
+	struct wlr_renderer* renderer;
+	struct wc_layer* layer;
+	struct timespec* when;
 };
 
-static void wc_render_surface(struct wlr_surface *surface,
-		struct wlr_output *output, struct wlr_renderer *renderer,
-		struct timespec *when, int sx, int sy, double ox, double oy) {
-	struct wlr_texture *texture = wlr_surface_get_texture(surface);
+static void wc_render_surface(struct wlr_surface* surface,
+		struct wlr_output* output, struct wlr_renderer* renderer,
+		struct timespec* when, int sx, int sy, double ox, double oy) {
+	struct wlr_texture* texture = wlr_surface_get_texture(surface);
 	if (texture == NULL) {
 		return;
 	}
@@ -56,11 +56,11 @@ static void wc_render_surface(struct wlr_surface *surface,
 	wlr_surface_send_frame_done(surface, when);
 }
 
-static void wc_render_view(struct wlr_surface *surface,
-		int sx, int sy, void *data) {
+static void wc_render_view(struct wlr_surface* surface,
+		int sx, int sy, void* data) {
 	struct wc_view_render_data* rdata = data;
-	struct wc_view *view = rdata->view;
-	struct wlr_output *output = rdata->output;
+	struct wc_view* view = rdata->view;
+	struct wlr_output* output = rdata->output;
 
 	double ox = 0, oy = 0;
 	wlr_output_layout_output_coords(
@@ -71,12 +71,12 @@ static void wc_render_view(struct wlr_surface *surface,
 			rdata->when, sx, sy, ox, oy);
 }
 
-static void wc_render_layer(struct wlr_surface *surface,
+static void wc_render_layer(struct wlr_surface* surface,
 		int sx, int sy, void* data) {
-	struct wc_layer_render_data *rdata = data;
-	struct wc_layer *layer = rdata->layer;
-	struct wc_server *server = layer->server;
-	struct wlr_output *output = layer->layer_surface->output;
+	struct wc_layer_render_data* rdata = data;
+	struct wc_layer* layer = rdata->layer;
+	struct wc_server* server = layer->server;
+	struct wlr_output* output = layer->layer_surface->output;
 
 	double ox = 0, oy = 0;
 	wlr_output_layout_output_coords(
@@ -87,10 +87,10 @@ static void wc_render_layer(struct wlr_surface *surface,
 			rdata->when, sx, sy, ox, oy);
 }
 
-static void wc_render_layers(struct timespec *now,
-		struct wlr_renderer *renderer, struct wc_output *output,
-		struct wl_list *layers) {
-	struct wc_layer *layer;
+static void wc_render_layers(struct timespec* now,
+		struct wlr_renderer* renderer, struct wc_output* output,
+		struct wl_list* layers) {
+	struct wc_layer* layer;
 	wl_list_for_each_reverse(layer, layers, link) {
 		if (!layer->mapped) {
 			continue;
@@ -106,11 +106,11 @@ static void wc_render_layers(struct timespec *now,
 	}
 }
 
-static void wc_output_frame(struct wl_listener *listener, void *data) {
-	struct wc_output *output = wl_container_of(listener, output, frame);
-	struct wc_server *server = output->server;
-	struct wlr_output *wlr_output = output->output;
-	struct wlr_renderer *renderer = wlr_backend_get_renderer(wlr_output->backend);
+static void wc_output_frame(struct wl_listener* listener, void* data) {
+	struct wc_output* output = wl_container_of(listener, output, frame);
+	struct wc_server* server = output->server;
+	struct wlr_output* wlr_output = output->output;
+	struct wlr_renderer* renderer = wlr_backend_get_renderer(wlr_output->backend);
 	assert(renderer);
 
 	struct timespec now;
@@ -128,20 +128,20 @@ static void wc_output_frame(struct wl_listener *listener, void *data) {
 	float color[4] = { 0.25f, 0.25f, 0.25f, 1 };
 	wlr_renderer_clear(renderer, color);
 
-	struct wl_list *backgrounds =
+	struct wl_list* backgrounds =
 		&output->layers[ZWLR_LAYER_SHELL_V1_LAYER_BACKGROUND];
-	struct wl_list *bottom =
+	struct wl_list* bottom =
 		&output->layers[ZWLR_LAYER_SHELL_V1_LAYER_BOTTOM];
-	struct wl_list *top =
+	struct wl_list* top =
 		&output->layers[ZWLR_LAYER_SHELL_V1_LAYER_TOP];
-	struct wl_list *overlay =
+	struct wl_list* overlay =
 		&output->layers[ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY];
 
 	wc_render_layers(&now, renderer, output, backgrounds);
 	wc_render_layers(&now, renderer, output, bottom);
 
 	// Render traditional shell surfaces between bottom and top layers.
-	struct wc_view *view;
+	struct wc_view* view;
 	wl_list_for_each_reverse(view, &server->views, link) {
 		if (!view->mapped) {
 			continue;
@@ -167,9 +167,9 @@ static void wc_output_frame(struct wl_listener *listener, void *data) {
 	wlr_output_swap_buffers(wlr_output, NULL, NULL);
 }
 
-static void wc_output_destroy(struct wl_listener *listener, void *data) {
-	struct wc_output *output = wl_container_of(listener, output, destroy);
-	struct wc_server *server = output->server;
+static void wc_output_destroy(struct wl_listener* listener, void* data) {
+	struct wc_output* output = wl_container_of(listener, output, destroy);
+	struct wc_server* server = output->server;
 	wl_list_remove(&output->link);
 
 	wl_list_remove(&output->frame.link);
@@ -186,17 +186,17 @@ static void wc_output_destroy(struct wl_listener *listener, void *data) {
 	free(output);
 }
 
-static void wc_new_output(struct wl_listener *listener, void *data) {
-	struct wc_server *server = wl_container_of(listener, server, new_output);
-	struct wlr_output *output = data;
+static void wc_new_output(struct wl_listener* listener, void* data) {
+	struct wc_server* server = wl_container_of(listener, server, new_output);
+	struct wlr_output* output = data;
 
 	if (!wl_list_empty(&output->modes)) {
-		struct wlr_output_mode *mode =
+		struct wlr_output_mode* mode =
 			wl_container_of(output->modes.prev, mode, link);
 		wlr_output_set_mode(output, mode);
 	}
 
-	struct wc_output *wc_output = calloc(1, sizeof(struct wc_output));
+	struct wc_output* wc_output = calloc(1, sizeof(struct wc_output));
 	wc_output->output = output;
 	wc_output->server = server;
 	output->data = wc_output;
@@ -224,18 +224,18 @@ static void wc_new_output(struct wl_listener *listener, void *data) {
 }
 
 
-struct wc_output *wc_get_active_output(struct wc_server *server) {
+struct wc_output* wc_get_active_output(struct wc_server* server) {
 	if (wl_list_empty(&server->outputs)) {
 		return NULL;
 	}
-	struct wc_output *output = server->active_output;
+	struct wc_output* output = server->active_output;
 	if (output == NULL) {
 		output = wl_container_of(server->outputs.prev, output, link);
 	}
 	return output;
 }
 
-void wc_init_output(struct wc_server *server) {
+void wc_init_output(struct wc_server* server) {
 	server->output_layout = wlr_output_layout_create();
 	wl_list_init(&server->outputs);
 	server->new_output.notify = wc_new_output;
