26419d7d3f86504477dab789f85d2f4761ff7a06
Timidger <APragmaticPlace@gmail.com>
2019-07-10 20:52:37 -0400

    Minimal working layer shell version

--- client/src/wayland_obj/mod.rs
+++ client/src/wayland_obj/mod.rs
@@ -1,11 +1,17 @@
 //! Wrappers around Wayland objects
 
+mod layer_shell;
 mod wl_compositor;
 mod wl_output;
 mod wl_shm;
+#[allow(dead_code)]
 mod xdg_shell;
 
 pub use self::{
+    layer_shell::{
+        create_layer_surface, layer_shell_init, Layer, LayerSurface,
+        LAYER_SHELL_VERSION
+    },
     wl_compositor::{
         create_surface, WlCompositorManager, WL_COMPOSITOR_VERSION
     },
