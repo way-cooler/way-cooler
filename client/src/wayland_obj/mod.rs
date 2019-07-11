4ed8bb6e21e1b789bd111adfa0e1885103b2e79f
Timidger <pcarpenter@starry.com>
2019-07-11 13:58:12 -0400

    Remove xdg shell

--- client/src/wayland_obj/mod.rs
+++ client/src/wayland_obj/mod.rs
@@ -4,8 +4,6 @@ mod layer_shell;
 mod wl_compositor;
 mod wl_output;
 mod wl_shm;
-#[allow(dead_code)]
-mod xdg_shell;
 
 pub use self::{
     layer_shell::{
@@ -16,8 +14,5 @@ pub use self::{
         create_surface, WlCompositorManager, WL_COMPOSITOR_VERSION
     },
     wl_output::{Output, WlOutputManager, WL_OUTPUT_VERSION},
-    wl_shm::{create_buffer, WlShmManager, WL_SHM_VERSION},
-    xdg_shell::{
-        create_xdg_toplevel, xdg_shell_init, XdgToplevel, XDG_WM_BASE_VERSION
-    }
+    wl_shm::{create_buffer, WlShmManager, WL_SHM_VERSION}
 };
